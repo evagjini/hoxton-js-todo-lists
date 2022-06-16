8c5ef61de99b6b4481e05cfd3e4137d96ca1eb3d
Fevronia Gjini <fevronigjini1@gmail.com>
2022-06-16 20:12:45 +0200

    this is the final

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// @ts-nocheck
+
 // - Create an alert that lists all users, with their ids, names and what city they're from
 // - Prompt the user for a user id and then:
 // - Display an alert with the username and all the todos titles that belong to that user 
@@ -17,13 +17,13 @@ alert(message)
 let id = Number(prompt("What is your id?"))
 console.log(id)
 
- for( user of users){
+ for( let user of users){
     if (id === user.id) 
     console.log("This is:"  + user.username)
  }
 
 
-for ( todo of todos){
+for ( let todo of todos){
     if( id === todo.userId)
     console.log(todo.title);
 }
